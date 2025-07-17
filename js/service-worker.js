a504e1d2eee3a5747ca14ff0df27dce45cb4bc41
Pokorski Sébastien <ervachats@gmail.com>
2025-07-17 20:21:22 +0200

    pwa

---
+++ js/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "stocker-cache-v1";
+const urlsToCache = [
+  "/stocker/",
+  "/stocker/login.html",
+  "/stocker/login.css",
+  "/stocker/login.js",
+  "/stocker/icons/icon-192x192.png",
+  "/stocker/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
