d03299d667b54a078151e1f52480ce88c66f4d65
fourcheerv <fourches.erv@gmail.com>
2026-01-30 06:47:22 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -158,3 +158,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+ const passwordInput = document.getElementById("passwordInput");
+  const togglePassword = document.getElementById("togglePassword");
+
+  togglePassword.addEventListener("click", () => {
+    const isPassword = passwordInput.type === "password";
+    passwordInput.type = isPassword ? "text" : "password";
+    togglePassword.textContent = isPassword ? "🙈" : "👁️";
+  });
